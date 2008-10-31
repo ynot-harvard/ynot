45a7e86289b5cd573f1eec358522591c59b80829
ryan <ryan@nesky>
2008-10-31 10:20:55 -0400

    merge for binomial trees

--- examples/BinomialHeap.v
+++ examples/BinomialHeap.v
@@ -5,6 +5,107 @@ Require Import Ynot.
 
 Set Implicit Arguments.
 
+Open Scope hprop_scope.
+Open Local Scope stsep_scope.
+Open Local Scope stsepi_scope.
+
+Section BINOMIAL_HEAP.
+
+Variable A:Set.
+Variable a:A. (* for testing *)
+
+(* A binomial heap is represented as a collection of binomial trees. *)
+
+Section BINOMIAL_TREE.
+
+(*  A binomial tree is defined recursively:
+    - a tree of order 0 is a single node
+    - a tree of order k has a root node whose children are
+      roots of binomial trees of orders k-1, k-2... 1, 0,
+      in this order. *)
+
+Fixpoint bltree (n: nat) : Type :=
+  match n with
+    | 0 => A
+    | S n' => prod ptr (bltree n')
+  end.
+
+Eval simpl in bltree 2.
+Definition t2 := (1, (0, a)) : bltree 2. 
+Definition t3 := (2, (1, (0, a))) : bltree 3.
+
+(* The modelling type is a binary tree. *)
+Fixpoint btree (n: nat) : Type:=
+  match n with
+    | 0 => A
+    | S n' => prod (btree n') (btree n')
+  end.
+
+Fixpoint rep' (n: nat) : bltree n -> btree n -> hprop :=
+ match n as n return bltree n -> btree n -> hprop with
+  | O    => fun a a' => [a = a']
+  | S n' => fun (bl: prod ptr (bltree n')) (bt: prod (btree n') (btree n')) =>
+              Exists bl' :@ bltree n', rep' n' (snd bl) (snd bt) * fst bl --> bl' * rep' n' bl' (fst bt)
+ end.  
+
+Definition rep (n: nat) (bt: btree n) (p: ptr) : hprop :=
+ Exists bl :@ bltree n, p --> bl * rep' n bl bt.
+
+Ltac t := unfold rep; unfold rep'; try match goal with 
+                                     | [ h : (?a, ?b) = (?c, ?d) |- _ ] => 
+                                       let y := fresh "y" in 
+                                       let z := fresh "z" in 
+                                         assert (a = c); try congruence; subst;  
+                                         assert (b = d); try congruence; subst; clear h 
+                                   end.
+
+Definition NewBl : forall hd, STsep __ (rep O hd).
+ intros. refine ( {{ New hd }} ); t; sep auto. Defined. 
+
+Definition FreeBl : forall (p: ptr) (hd: [btree O]),
+ STsep (hd ~~ rep 0 hd p) (fun _:unit => __).
+ intros. refine ( {{ Free p }} ); t; sep auto. Defined. 
+
+(* In these specs, n is not computationally irrelevent. *)
+
+Parameter ff : False.
+
+
+Definition members :        forall (n: nat) (bt: btree n) (bl: bltree n), STsep (rep' n bl bt) (fun r: btree n => rep' n bl bt * [r = bt]).
+ refine (fix F (n: nat) {struct n} : forall (bt: btree n) (bl: bltree n), STsep (rep' n bl bt) (fun r: btree n => rep' n bl bt * [r = bt])   := 
+            match n as n return      forall (bt: btree n) (bl: bltree n), STsep (rep' n bl bt) (fun r: btree n => rep' n bl bt * [r = bt])  with 
+              | O => fun (bt: A) (bl: A) => {{ SepReturn bl <@> _ }}
+              | S n' => fun (bt: prod (btree n') (btree n')) (bl: prod ptr (bltree n')) => 
+                          match bl return STsep (rep' (S n') bl bt) (fun r: btree (S n') => rep' (S n') bl bt * [r = bt]) with
+                            | (p1, bl1) => bt1 <- F n' (snd bt) bl1 <@> rep n' (fst bt) p1 ;
+                                           bl2 <- p1 !! fun bl2 => rep n' (fst bt) p1  ;
+                                           bt2 <- F n' (snd bt) bl2 <@> _ ; 
+                                           {{ SepReturn (bt2, bt1)  <@> _ }} end end). Admitted.   
+
+(* These proofs can take up to 20 seconds on my laptop *)
+
+(* Merging is attaching one tree as the leftmost child of the other.
+   This spec, which updates in place, requires strong update, because the type
+   of the node changes (it increases by one level)  *)
+Conjecture merge : forall (n: nat) (p1 p2: ptr) (bt1 bt2: btree n), 
+ STsep (rep n bt1 p1 * rep n bt2 p2)
+       (fun r:unit => rep (S n) (bt1, bt2) p2).
+(* intros. refine ( oldp2node <- p2 !! (fun oldp2node:bltree n => rep n p1 bt1 * rep' n oldp2node bt2) ;
+                  {{ p2 ::= (p1, oldp2node) }} ). *)
+ 
+(* Update in place can be done by switching to a 
+   bltree that returns a Set rather than a Type.
+   Or just create a new pointer. *)
+Definition merge' : forall (p1 p2: ptr) (n: nat) (bt1 bt2: [btree n]), 
+ STsep ((bt1 ~~ rep n bt1 p1) * (bt2 ~~ rep n bt2 p2))
+       (fun ps => bt1 ~~ bt2 ~~ Exists x :@ bltree n, 
+                    p2 --> x * ps --> (p1, x) * 
+                    rep' (S n) (p1, x) (bt1, bt2)  ).
+intros. refine ( x <- p2 !! (fun x => (bt1 ~~ rep n bt1 p1) * (bt2 ~~ rep' n x bt2)) ;
+                 {{ New (p1, x) }} ); try solve [ induction n; t; sep auto | t; sep auto]. Defined.  
+
+End BINOMIAL_TREE.
+End BINOMIAL_HEAP.
 (* Some helpers *)
 Section T.
  Variables (T: Set) 
@@ -182,86 +283,4 @@ Axiom rep_sorted : forall q ls h, rep q ls h -> sorted ord ls.
 End PRIORITY_QUEUE.
 End PRIORITY_QUEUE_3.
 
-Section BINOMIAL_TREE.
 
-Variable A:Set.
-Variable a:A. (* for testing *)
-
-(* From wikipedia:
-   A binomial heap is represented as a collection of binomial
-   trees.  A binomial tree is defined recursively:
-    - a tree of order 0 is a single node
-    - a tree of order k has a root node whose children are
-      roots of binomial trees of orders k-1, k-2... 1, 0,
-      in this order.
-*)
-
-(* The element associated with each tree is at
-   the *end* of the list of trees. *)
-Fixpoint btree (n: nat) : Type:=
-  match n with
-    | 0 => A
-    | S n' => prod ptr (btree n')
-  end.
-
-Eval simpl in btree 2.
-Definition t2 := (1, (0, a)) : btree 2. 
-Definition t3 := (2, (1, (0, a))) : btree 3.
-
-Open Scope hprop_scope.
-
-Fixpoint rep' (n: nat) {struct n} : btree n -> hprop :=
- match n as n return btree n -> hprop with
-  | 0 => fun (a0: A) => [a0 = a0] (* helps with counting elements *)
-  | S n' => fun (bt: prod ptr (btree n')) => rep' n' (snd bt) *
-             (Exists bt' :@ btree n', fst bt --> bt' * rep' n' bt')
- end. 
-
-Definition rep (n: nat) (p: ptr) : hprop :=
- Exists bt :@ btree n, p --> bt.
-
-Eval simpl in rep' 2 t2.
-(*  = [a = a] * (Exists bt' :@ A, 0 --> bt' * [bt' = bt']) *
-       (Exists bt' :@ ptr * A,
-        1 --> bt' *
-        ([snd bt' = snd bt'] *
-         (Exists bt'0 :@ A, fst bt' --> bt'0 * [bt'0 = bt'0])))
-     : hprop  *)
-
-(* Attach one as the leftmost child of the other *)
-Conjecture merge : forall (n: nat) (p1 p2: ptr), 
- STsep (rep n p1 * rep n p2)
-       (fun r:unit => rep (S n) p1).
- (* this is too weak, need to rep a btree n with a list of length 2^(n+1) *)
- (* in retrospect,don't need to index with n *)
-Inductive ilist : nat -> Set :=
- | inil : A -> ilist 1
- | icons : forall n, ilist n -> ilist n -> ilist (n+n).
-
-(* Fixpoint rep0' (n: nat) (ls: ilist (S n)) {struct ls} : btree n -> hprop :=
- match ls in ilist n0 return btree  with
-  | inil a0 => [True]
-  | icons n0 l1 l2 => fun _ => [True]
- end.
-
-Fixpoint rep0' (n: nat) {struct n} : btree n -> ilist (S n) -> hprop :=
- match n as n return btree n -> ilist (S n) -> hprop with
-  | 0 => fun (a0: A) (ls: ilist 1) => [ls = inil a0] (* helps with counting elements *)
-  | S n' => fun (bt: prod ptr (btree n')) (ls: ilist (S (S n')))  => 
-      match ls in ilist n0 with
-       | inil _ => [False]
-       | icons n0 l1 l2 => rep0' (snd bt) l1 * (Exists bt' :@ btree n', fst bt --> bt' * rep0' n' bt')
-      end
- end. 
-
-Eval simpl in rep0' 2 t2.
-*)
-
-End BINOMIAL_TREE.
-
-(* 
-*** Local Variables: ***
-*** coq-prog-name: "c:/Coq/bin/coqtop.opt.exe" ***
-*** coq-prog-args: ("-emacs-U" "-R" "c:/ynot/src/" "Ynot" ) ***
-*** End: ***
- *)
