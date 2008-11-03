2ae68f3cba8b8648f7cc13d9d08bbd9d958e231a
shinnar <devnull@localhost>
2008-11-03 06:44:31 -0500

    added in if forms for sumbool, sumor.  now have dependent Fix[234]

--- src/Case.v
+++ src/Case.v
@@ -21,3 +21,53 @@ Notation "'IfNull' x 'Then' e1 'Else' e2" :=
 Notation "'IfNull' e 'As' x 'Then' e1 'Else' e2" :=
   (ocase e (fun _ => e1) (fun x _ => e2))
   (no associativity, at level 90).
+
+Section sbcase.
+  Variables P Q:Prop.
+  Variables B : Type.
+
+  Variable disc : {P} + {Q}.
+
+  Variable pCase : forall v, disc = left Q v -> B.
+  Variable qCase : forall v, disc = right P v -> B.
+
+  Definition sbcase :=
+    match disc as disc' return (disc = disc' -> B) with
+      | left v => @pCase v
+      | right v => @qCase v
+    end (refl_equal _).
+End sbcase.
+
+Implicit Arguments sbcase [P Q B].
+
+Notation "'IfSB' x 'Then' e1 'Else' e2" :=
+  (sbcase x (fun x _ => e1) (fun x _ => e2))
+  (no associativity, at level 90).
+Notation "'IfSB' e 'As' x 'Then' e1 'Else' e2" :=
+  (sbcase e (fun x _ => e1) (fun x _ => e2))
+  (no associativity, at level 90).
+Section socase.
+  Variable P:Type.
+  Variable Q:Prop.
+  Variable B : Type.
+
+  Variable disc : P + {Q}.
+
+  Variable pCase : forall v, disc = inleft Q v -> B.
+  Variable qCase : forall v, disc = inright P v -> B.
+
+  Definition socase :=
+    match disc as disc' return (disc = disc' -> B) with
+      | inleft v => @pCase v
+      | inright v => @qCase v
+    end (refl_equal _).
+End socase.
+
+Implicit Arguments socase [P Q B].
+
+Notation "'IfSO' x 'Then' e1 'Else' e2" :=
+  (socase x (fun x _ => e1) (fun x _ => e2))
+  (no associativity, at level 90).
+Notation "'IfSO' e 'As' x 'Then' e1 'Else' e2" :=
+  (socase e (fun x _ => e1) (fun x _ => e2))
+  (no associativity, at level 90).
