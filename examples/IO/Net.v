1f4a4ac0308713089a45b705470a75b4fbf423ed
Gregory Malecha <gmalecha@eecs.harvard.edu>
2009-02-18 22:57:59 -0500

    Working on migrating stuff from web-server into main branch.

---
+++ examples/IO/Net.v
@@ -0,0 +1,121 @@
+Require Import Ynot.
+Require Import IO FS.
+Require Import List Ascii.
+
+Set Implicit Arguments.
+
+Open Local Scope stsepi_scope.
+Open Local Scope hprop_scope.
+
+(** Socket Address **)
+Axiom axiom_SockAddr : Set.
+Definition SockAddr := axiom_SockAddr.
+
+(** constructor for socket addresses **)
+Axiom axiom_sockaddr : forall (n1 n2 n3 n4 : sig (fun x : nat => x <= 255))
+  (port : nat),
+  STsep (__)
+        (fun r : SockAddr => hprop_empty).
+Definition sockaddr := axiom_sockaddr.
+
+Axiom axiom_sock_eq : forall (sa1 sa2 : SockAddr), {sa1 = sa2} + {sa1 <> sa2}.
+Definition sock_eq := axiom_sock_eq.
+
+(** UDP Sockets **)
+Axiom axiom_UDP_Sent : SockAddr -> SockAddr -> list ascii -> Action.
+Axiom axiom_UDP_Recd : SockAddr -> SockAddr -> list ascii -> Action.
+Axiom axiom_UDP_send : forall (local remote: SockAddr) (s: list ascii) (tr: [Trace]),
+  STsep (tr ~~ traced tr)
+        (fun r:unit => tr ~~ traced (axiom_UDP_Sent local remote s :: tr)).   
+Axiom axiom_UDP_recv : forall (local: SockAddr) (tr: [Trace]),
+  STsep (tr ~~ traced tr) 
+        (fun r => tr ~~  traced (axiom_UDP_Recd local (fst r) (snd r) :: tr)).
+
+Module UDP.
+
+  Definition Sent := axiom_UDP_Sent.
+  Definition Recd := axiom_UDP_Recd.
+
+  Definition send : forall (local remote: SockAddr) (s: list ascii) (tr: [Trace]),
+    STsep (tr ~~ traced tr)
+          (fun r:unit => tr ~~ traced (Sent local remote s :: tr)) :=
+    axiom_UDP_send.  
+  Definition recv : forall (local: SockAddr) (tr: [Trace]),
+    STsep (tr ~~ traced tr) 
+          (fun r => tr ~~  traced (Recd local (fst r) (snd r) :: tr)) :=
+    axiom_UDP_recv.
+
+End UDP.
+
+(** TCP Sockets **)
+Axiom axiom_BoundSocketModel : SockAddr -> SockAddr -> fd_model.
+Definition BoundSocketModel := axiom_BoundSocketModel.
+Axiom axiom_ListenSocketModel : SockAddr -> fd_model.
+Definition ListenSocketModel := axiom_ListenSocketModel.
+
+Axiom axiom_TCP_Accept : SockAddr -> SockAddr -> SockAddr -> Action.
+
+Axiom axiom_TCP_listenSocket : forall (skt : SockAddr),
+  STsep (__)
+        (fun fd:(File (ListenSocketModel skt) (R :: nil)) => handle fd).
+
+Definition rt := sigT (fun x:(SockAddr * SockAddr) => (File (BoundSocketModel (fst x) (snd x)) (R :: W :: nil))).
+
+Axiom axiom_TCP_bindSocket : forall (local remote : SockAddr),
+  STsep (__)
+        (fun fd:File (BoundSocketModel local remote) (R :: W :: nil) => handle fd).
+
+Axiom axiom_TCP_accept : forall (skt : SockAddr) (fd : File (ListenSocketModel skt) (R :: nil))
+  (tr : [Trace]),
+  STsep (tr ~~ traced tr)
+        (fun r:rt => tr ~~ handle (projT2 r) *
+            traced (axiom_TCP_Accept (fst (projT1 r)) (snd (projT1 r)) skt :: tr)).
+
+Module TCP.
+
+  Definition Accept := axiom_TCP_Accept.
+
+  Definition listenSocket := axiom_TCP_listenSocket.
+  Definition bindSocket := axiom_TCP_bindSocket.
+  Definition accept : forall (skt : SockAddr) 
+    (fd : File (ListenSocketModel skt) (R :: nil)) (tr : [Trace]),
+    STsep (tr ~~ traced tr)
+          (fun r:rt => tr ~~ handle (projT2 r) *
+              traced (Accept (fst (projT1 r)) (snd (projT1 r)) skt :: tr)) :=
+      axiom_TCP_accept.
+
+End TCP.
+
+(** SSL Sockets **)
+Axiom axiom_SslListenSocketModel : SockAddr -> fd_model.
+Definition SslListenSocketModel := axiom_SslListenSocketModel.
+
+Axiom axiom_SSL_secure : forall (m : fd_model) (ms : list mode), File m ms -> Prop.
+Definition secure := axiom_SSL_secure.
+
+Axiom axiom_SSL_listenSocket : forall (skt : SockAddr),
+  STsep (__)
+        (fun fd:(File (SslListenSocketModel skt) (R :: nil)) => handle fd * [secure fd]).
+
+Axiom axiom_SSL_bindSocket : forall (local remote : SockAddr),
+  STsep (__)
+        (fun fd:File (BoundSocketModel local remote) (R :: W :: nil) => handle fd * [secure fd]).
+
+Axiom axiom_SSL_accept : forall (skt : SockAddr) (fd : File (SslListenSocketModel skt) (R :: nil))
+  (tr : [Trace]),
+  STsep (tr ~~ traced tr * [secure fd])
+        (fun r:rt => tr ~~ handle (projT2 r) * [secure fd] * [secure (projT2 r)] *
+            traced (axiom_TCP_Accept (fst (projT1 r)) (snd (projT1 r)) skt :: tr)).
+
+Module SSL.
+
+  Definition listenSocket := axiom_SSL_listenSocket.
+  Definition bindSocket := axiom_SSL_bindSocket.
+  Definition accept : forall (skt : SockAddr) 
+    (fd : File (SslListenSocketModel skt) (R :: nil)) (tr : [Trace]),
+    STsep (tr ~~ traced tr * [secure fd])
+          (fun r:rt => tr ~~ handle (projT2 r) * [secure fd] * [secure (projT2 r)] *
+              traced (TCP.Accept (fst (projT1 r)) (snd (projT1 r)) skt :: tr)) :=
+      axiom_SSL_accept.
+
+End SSL.
--- src/coq/Basis.v
+++ src/coq/Basis.v
@@ -32,17 +32,82 @@ Require Import Ynot.Hprop.
 
 Require Import String.
 Require Import List.
+Require Import Ascii.
 
 Open Local Scope hprop_scope.
 
+(** String <-> List Conversion **)
+Fixpoint str2la (str : string) : list ascii :=
+  match str with
+    | EmptyString => nil
+    | String a b => a :: (str2la b)
+  end.
+
+Fixpoint la2str (la : list ascii) : string :=
+  match la with
+    | nil => EmptyString
+    | a :: b => String a (la2str b)
+  end.
+
+Theorem str2la_inv_la2str : forall str, la2str (str2la str) = str.
+  induction str; [ auto | simpl; rewrite IHstr; trivial ].
+Qed.
+
+Theorem la2str_inv_str2la : forall str, str2la (la2str str) = str.
+  induction str; [ auto | simpl; rewrite IHstr; trivial ].
+Qed.
+
+(** Standard "println" **)
 Axiom printString : forall (s : string),
   STsep (__) (fun _:unit => __).
 
-Axiom printStringLn : forall (s : string),
-  STsep (__) (fun _:unit => __).
+Definition printStringLn (s : string) :
+  STsep (__) (fun _:unit => __) :=
+  printString (s ++ (String "010"%char (String "013"%char ""%string))).
 
-Axiom printString' : forall (s : list Ascii.ascii),
-  STsep (__) (fun _:unit => __).
+Definition printString' (s : list ascii) :
+  STsep (__) (fun _:unit => __) :=
+  printString (la2str s).
 
-Axiom printStringLn' : forall (s : list Ascii.ascii),
-  STsep (__) (fun _:unit => __).
+Definition printStringLn' (s : list Ascii.ascii) :
+  STsep (__) (fun _:unit => __) :=
+  printStringLn (la2str s).
+
+(** Numbers -> Strings **)
+
+Open Local Scope char_scope.
+
+Definition dd n :=
+  match n with
+    | 0 => "0" | 1 => "1" | 2 => "2" | 3 => "3" | 4 => "4"
+    | 5 => "5" | 6 => "6" | 7 => "7" | 8 => "8" | 9 => "9"
+    | _ => "x"
+  end.
+
+ Fixpoint div_mod_10 (a q : nat) {struct a} :=
+   match a with
+     | 0 => (q,0)
+     | 1 => (q,1)
+     | 2 => (q,2)
+     | 3 => (q,3)
+     | 4 => (q,4)
+     | 5 => (q,5)
+     | 6 => (q,6)
+     | 7 => (q,7)
+     | 8 => (q,8)
+     | 9 => (q,9)
+     | S (S (S (S (S (S (S (S (S (S n))))))))) => div_mod_10 n (S q)
+   end.
+
+ Fixpoint ntos (x : nat) (n : nat) (l : list ascii) {struct x} :=
+   match n with
+     | 0 => match l with
+              | nil => "0" :: l
+              | _ => l
+            end
+     | _ => let (q,r) := div_mod_10 n 0 in
+       match x with
+         | 0 => "t" :: "o" :: "o" ::" " :: "b" :: "i" :: "g" :: nil
+         | S x' => ntos x' q (dd r :: l)
+       end
+   end.
